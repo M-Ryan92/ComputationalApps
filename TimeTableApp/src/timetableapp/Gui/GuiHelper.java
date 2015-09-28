b9d522578408b205a3e99061c251e32d675ba924
dolstrm001 <michel.dolstra@gmail.com>
2015-09-28 17:42:16 +0200

    added lombok lib, dialog gui and appstate util

--- TimeTableApp/src/timetableapp/Gui/GuiHelper.java
+++ TimeTableApp/src/timetableapp/Gui/GuiHelper.java
@@ -3,7 +3,10 @@ package timetableapp.Gui;
 import controlP5.ControlEvent;
 import controlP5.ControlP5;
 import controlP5.Controller;
+import java.awt.Frame;
+import javax.swing.JOptionPane;
 import processing.core.PApplet;
+import timetableapp.util.AppState;
 
 public class GuiHelper {
 
@@ -21,13 +24,26 @@ public class GuiHelper {
                 .setPosition(10, app.height - btnheight - 10)
                 .setSize(70, btnheight)
                 .setLabel("Select File");
-
+        cp5.addButton(cp5, "viewData")
+                .setPosition(10, app.height - (btnheight * 2) - 20)
+                .setSize(70, btnheight)
+                .setLabel("View Data");
     }
 
     public void controlEvent(ControlEvent evt) {
         Controller<?> controller = evt.getController();
-        if ("selectFileBtn".equals(controller.getName())) {
+        switch(controller.getName()){
+        case("selectFileBtn"):
             app.selectInput("Select a file to process:", "fileSelected");
+            break;
+        case("viewData"):
+            if(AppState.FILE_LOADED == 0){
+                new Dialog(null,"No File Selected", Dialog.WARNING_MESSAGE);
+            } else {
+                //showmydataTable(pagina's)
+                
+            }
+            break;
         }
 
     }
---
+++ TimeTableApp/src/timetableapp/util/AppState.java
@@ -0,0 +1,10 @@
+package timetableapp.util;
+
+import lombok.Data;
+
+@Data public class AppState {
+    public static int FILE_SELECTED = 0;
+    public static int LOADING_FILE = 0;
+    public static int FILE_LOADED = 0;
+    public static int SELECTED_VIEW = 0;        
+}
