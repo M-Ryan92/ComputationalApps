719a1849f2fcb359c4a4eaf74e3f31c95828ca52
dolstrm001 <michel.dolstra@hva.nl>
2015-09-28 13:26:19 +0200

    setup base application

---
+++ TimeTableApp/TimeTableApp/src/timetableapp/TimeTableApp.java
@@ -0,0 +1,16 @@
+package timetableapp;
+
+import processing.core.PApplet;
+
+public class TimeTableApp {
+
+    public static void main(String[] args) {
+        PApplet app = new BaseApplication();
+        
+        app.main(new String[]{
+            //"--present", // makes the app appear in fullscreen mode
+            app.getClass().getName()
+        });
+    }
+
+}
