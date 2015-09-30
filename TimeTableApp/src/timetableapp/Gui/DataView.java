f43d362fc13d0f36740b08ac14a170ebde6f4ba8
dolstrm001 <michel.dolstra@gmail.com>
2015-09-30 16:14:38 +0200

    add table draw logic needs some refactoring but TODO remove row from Tablemodel and replace it with some basic data from the row

--- TimeTableApp/src/timetableapp/Gui/DataView.java
+++ TimeTableApp/src/timetableapp/Gui/DataView.java
@@ -1,7 +1,10 @@
 package timetableapp.Gui;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import processing.core.PApplet;
+import processing.data.TableRow;
 import timetableapp.models.DataManager;
 
 public class DataView extends BaseView {
@@ -22,27 +25,47 @@ public class DataView extends BaseView {
         }
     }
 
+    private int page = 5;
+
     @Override
     public void draw() {
         if (ishidden == false) {
-            
+
             app.translate(20, 20);
             int offset = 20;
             app.rect(0, 0, app.width - 40, app.height - 170);
-            
+
             app.textAlign(PApplet.CENTER);
-            
+
             dm.getTm().getColumns().forEach(s -> {
                 String txt = (String) s;
-                int width = (int)app.textWidth(txt) + 10;
+                int width = (int) app.textWidth(txt) + 10;
+                int rowheight = btnheight - 7;
+                for (TableRow row : (List<TableRow>) dm.getTm().getPages().get(page)) {
+                    if (row.getString(txt) != null) {
+                        int nw = (int) app.textWidth(row.getString(txt)) + 10;
+                        if (nw > width) {
+                            width = nw;
+                        }
+                    }
+                    app.fill(0);
+                    rowheight += btnheight;
+
+                    String colValue = row.getString(txt) != null ? row.getString(txt) : "";
+                    app.text(colValue, width - (width / 2), rowheight);
+                    int lineheight = rowheight + 7;
+                    app.line(0, lineheight, app.width - 20, lineheight);
+                    
+                    app.fill(255);
+                }
+
                 app.rect(0, 0, width, btnheight);
                 app.line(width, 20, width, app.height - 170);
                 app.fill(0);
-                app.text(txt, width - (width/2), btnheight -7);
+                app.text(txt, width - (width / 2), btnheight - 7);
                 app.fill(255);
                 app.translate(width, 0);
             });
-            app.translate(0, 0);
 
         }
     }
