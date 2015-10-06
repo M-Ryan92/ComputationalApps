108b6de12cd1d5dc40b2f36ad9972f5937a367e7
dolstrm001 <michel.dolstra@gmail.com>
2015-10-06 23:14:19 +0200

    some small changes to the model

--- TimeTableApp/src/timetableapp/models/Activity.java
+++ TimeTableApp/src/timetableapp/models/Activity.java
@@ -1,6 +1,5 @@
 package timetableapp.models;
 
-import java.sql.Time;
 import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.Date;
@@ -9,28 +8,36 @@ import java.util.Locale;
 public class Activity {
 
     //what
-    String activity;
-    String course;
-    
+    private String activity;
+    private String course;
+
     //when
-    int weekNr;
-    String day;
-    Date startDate;
-    Date endDate;
-    Date startTime;
-    Date endTime;
-    Time duration;
-    
+    private int weekNr;
+    private String day;
+    private Date startDate;
+    private Date endDate;
+    private Date startTime;
+    private Date endTime;
+    private int[] duration = new int[3];
+
     public Activity(String activity, String course, Date startDay, Date endDay) {
         this.activity = activity;
         this.course = course;
         this.startDate = startDay;
         this.endDate = endDay;
+        this.startTime = startDay;
+        this.endTime = endDay;
+
+        long diff = endTime.getTime() - startTime.getTime();
+        duration[0] = (int) (diff / (24 * 60 * 60 * 1000));//days
+        duration[1] = (int) (diff / (60 * 60 * 1000));//hours
+        duration[2] = (int) (diff / (60 * 1000));//minutes
+
         Calendar cal = Calendar.getInstance();
         cal.setTime(startDate);
-        
+
         day = new DateFormatSymbols(Locale.ENGLISH).getWeekdays()[cal.get(Calendar.DAY_OF_WEEK)];
         weekNr = cal.get(Calendar.WEEK_OF_YEAR);
     }
-    
+
 }
